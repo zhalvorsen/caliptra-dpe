6f5de80969b481f3ad44fca8f6e63e2f93822ec2
Zach Halvorsen <zhalvorsen@google.com>
2026-03-16 22:57:31 +0000

    Make `Hasher` dyn compatible
    This converts the `Hasher` struct to no longer be `Sized` which will make it easier to start making the `Crypto` trait dyn compatible.

--- dpe/src/commands/derive_context.rs
+++ dpe/src/commands/derive_context.rs
@@ -459,7 +459,7 @@ mod tests {
         DpeProfile, MAX_CERT_SIZE, MAX_EXPORTED_CDI_SIZE, MAX_HANDLES, TCI_SIZE,
     };
     use caliptra_cfi_lib::CfiCounter;
-    use caliptra_dpe_crypto::{Crypto, Hasher};
+    use caliptra_dpe_crypto::Crypto;
     use caliptra_dpe_platform::{Platform, MAX_KEY_IDENTIFIER_SIZE};
     use openssl::{
         bn::BigNum,
@@ -1062,14 +1062,22 @@ mod tests {
         assert!(env.state.contexts[child_idx].allow_export_cdi());
 
         // check tci_cumulative correctly computed
-        let mut hasher = env.crypto.hash_initialize().unwrap();
-        hasher.update(&[0u8; DPE_PROFILE.hash_size()]).unwrap();
-        hasher.update(&[1u8; DPE_PROFILE.hash_size()]).unwrap();
-        let temp_digest = hasher.finish().unwrap();
-        let mut hasher_2 = env.crypto.hash_initialize().unwrap();
-        hasher_2.update(temp_digest.as_slice()).unwrap();
-        hasher_2.update(&[2u8; DPE_PROFILE.hash_size()]).unwrap();
-        let digest = hasher_2.finish().unwrap();
+        let temp_digest = env
+            .crypto
+            .with_hasher(&|hasher| {
+                hasher.update(&[0u8; DPE_PROFILE.hash_size()]).unwrap();
+                hasher.update(&[1u8; DPE_PROFILE.hash_size()]).unwrap();
+                Ok(())
+            })
+            .unwrap();
+        let digest = env
+            .crypto
+            .with_hasher(&|hasher| {
+                hasher.update(temp_digest.as_slice()).unwrap();
+                hasher.update(&[2u8; DPE_PROFILE.hash_size()]).unwrap();
+                Ok(())
+            })
+            .unwrap();
         assert_eq!(
             digest.as_slice(),
             env.state.contexts[child_idx].tci.tci_cumulative.0
